6e9d9b1dbdbac402de385342c8d01b8ce7b06740
Leandro <lfunes@riocuarto.gov.ar>
2026-07-13 20:21:24 -0300

    feat: add root layout with font configuration, Google Tag Manager integration, and core components

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Inter, Roboto } from "next/font/google";
-
+import Script from "next/script";
 import "bootstrap/dist/css/bootstrap.min.css"; 
 import "bootstrap-icons/font/bootstrap-icons.css"; // Importamos icons por separado para asegurar visibilidad
 import "@/styles/globals.scss";
@@ -35,6 +35,27 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es" className={`${inter.variable} ${roboto.variable}`}>
       <body className="d-flex flex-column min-vh-100">
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-MF9KXZNK`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MF9KXZNK');
+            `,
+          }}
+        />
         <NavBar />
         <main className="flex-grow-1">
           {children}
